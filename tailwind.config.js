ffeda6c6750de88c938efa02b6544bf92a2b2105
Kshitij Kumar <kumarkshitij171@gmail.com>
2023-07-24 22:05:47 +0530

    making button functional

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./dist/*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Ubuntu' : ['Ubuntu', 'sans-serif'],
+        'Varela Round': ['Varela Round', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
